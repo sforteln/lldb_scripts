20228b173d7f38d2f7a3e06124ca486f978e7f8f
Simon Fortelny <smf@iteris.com>
2016-07-31 14:51:25 -0400

    Add the ability to show the output of a lldb command in any app

--- capture_output.py
+++ capture_output.py
@@ -3,6 +3,7 @@
 #command script import ~/git/lldb_scripts/lldb_utils.py
 #script reload(lldb_utils)
 #script help(lldb.SBFrame)
+#log enable -f /tmp/lldb-log.txt lldb api process
 
 import lldb
 import time
@@ -20,25 +21,39 @@ def copy_to_paste_buffer(debugger, command, result, internal_dict):
     command = "po {0}".format(command)
   interpreter.HandleCommand(command, res)
   if res.Succeeded():
-    print "result of '{0}' is now in copy buffer".format(command)
+    #print >>result,"result of '{0}' is now in copy buffer".format(command)
     copy_cmd = "echo '{0}' | pbcopy".format(res.GetOutput())
     subprocess.call(copy_cmd, shell=True)
   else :
-    print "Command failed"
+    print >>result,"Command failed"
 
 def view_in_xcode(debugger, command, result, internal_dict):
   """This command writes the commands output into a temp file and opens it in xcode """
-  filename = "/tmp/{0}".format(uuid.uuid4())
-  result = write_cmd_to_file(debugger,command, filename)
+  view_in("Xcode.app",command,debugger,result, internal_dict)
+
+def view_in(debugger,command,result, internal_dict):
+  """This command writes the commands output into a temp file and opens it in xcode """
+  temp_filename = "/tmp/{0}".format(uuid.uuid4())
+  parser = view_in_commands_parser()
+  (options, args) = parser.parse_args(shlex.split(command))
+  if not options.cmd:
+      result.PutCString("the command(-c) is required")
+      return
+  result = write_cmd_to_file(debugger,options.cmd, temp_filename)
   if result:
     # open with xcode
-    subprocess.Popen(["open" ,"-a" ,"Xcode.app",filename])
-
+    subprocess.Popen(["open" ,"-a" ,options.app,temp_filename])
+   
 
 def write_to_file(debugger, command, result, internal_dict):
   """This command takes a lldb command and a file to write it to
   Ex. wf -f /tmp/lldb_output -c 'po self' """
-  options = ParseWriteToFileCommands(command) 
+  
+  parser = write_to_file_commands_parser()
+  (options, args) = parser.parse_args(shlex.split(command))
+  if not options.cmd:
+      result.PutCString("the command(-c) is required")
+      return
   write_cmd_to_file(debugger,options.cmd,options.file)
 
 def write_cmd_to_file(debugger,command, filename):
@@ -47,7 +62,7 @@ def write_cmd_to_file(debugger,command, filename):
   if command.startswith('p')==False :
     command = "po {0}".format(command)
   interpreter.HandleCommand(command, res)
- 
+  
   if res.Succeeded():
     print "wrote {0} to {1}".format(command,filename)
     cmd_result = res.GetOutput()
@@ -58,12 +73,17 @@ def write_cmd_to_file(debugger,command, filename):
     print "Command failed"
     return False
 
-def ParseWriteToFileCommands(command):
+def view_in_commands_parser():
+  parser = OptionParser()
+  parser.add_option("-a", "--app", action="store", type="string", dest="app", default="Xcode.app" ,help="The app to open the request in 'open -a <tempfile>' [default]")
+  parser.add_option("-c", "--cmd", action="store", type="string", dest="cmd",help="the lldb command to write to the file [default]")
+  return parser
+
+def write_to_file_commands_parser():
   parser = OptionParser()
   parser.add_option("-f", "--file", action="store", type="string", dest="file", default="/tmp/lldb_output" ,help="The file to write the results into [default]")
-  parser.add_option("-c", "--cmd", action="store", type="string", dest="cmd",default="po self" ,help="the lldb command to write to the file [default]")
-  (options, args) = parser.parse_args(shlex.split(command))
-  return options
+  parser.add_option("-c", "--cmd", action="store", type="string", dest="cmd",help="the lldb command to write to the file [default]")
+  return parser
 
 def __lldb_init_module(debugger, internal_dict):
   debugger.HandleCommand('command script add -f capture_output.copy_to_paste_buffer copy_to_paste_buffer')
@@ -74,4 +94,5 @@ def __lldb_init_module(debugger, internal_dict):
   print 'The "write_to_file"(wf) command has been installed and is ready of use.'
   debugger.HandleCommand('command script add -f capture_output.view_in_xcode view_in_xcode')
   debugger.HandleCommand('command script add -f capture_output.view_in_xcode vx')
+  debugger.HandleCommand('command script add -f capture_output.view_in vi')
   print 'The "view_in_xcode"(vx) command has been installed and is ready of use.'
